6f2194fc5eb29031e50314649bb9eda05eba37b9
Josenberg <mbx.philipe@gmail.com>
2014-12-23 00:48:56 -0200

    add function of add new websites from gui

--- app/controllers/websites_controller.rb
+++ app/controllers/websites_controller.rb
@@ -1,5 +1,29 @@
 class WebsitesController < ApplicationController
+  def new
+    @website = Website.new
+  end
+
+  def create
+    @website = Website.new(website_params)
+    respond_to do |format|
+      if @website.save
+        format.html { redirect_to @website, notice: 'website was successfully created.' }
+        format.json { render :show, status: :created, location: @website }
+      else
+        format.html { render :new }
+        format.json { render json: @website.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
   def index
     @websites = Website.all
   end
+
+  private
+
+  def website_params
+    params.require(:website).permit(:url, :description)
+  end
+
 end
